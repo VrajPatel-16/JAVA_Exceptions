fb8c2947db088edcbc9bcd60b7e92415ef805d66
Vraj Patel <vrajbpatel1719@gmail.com>
2025-04-19 01:13:14 +0530

    Initialize main class with main method structure

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ 
+ public class Main {
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         Operations op = new Operations();
+         boolean exit = false;
+ 
+         
+     }
+ }
